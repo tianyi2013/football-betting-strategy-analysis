fb5114bb639d7ed8c38f59729fe3de5e98456922
Tianyi Chen <mr.tianyichen@gmail.com>
2025-10-21 21:16:57 +0100

    updated results

---
+++ cleansing.bat
@@ -0,0 +1,5 @@
+python main.py --league premier_league --command cleanse
+python main.py --league bundesliga_1 --command cleanse
+python main.py --league laliga_1 --command cleanse
+python main.py --league le_championnat --command cleanse
+python main.py --league serie_a --command cleanse
---
+++ prediction.bat
@@ -0,0 +1,5 @@
+python main.py --league premier_league --command predict >> premier_league.txt
+python main.py --league bundesliga_1 --command predict >> budesliga.txt
+python main.py --league laliga_1 --command predict >> laliga.txt
+python main.py --league le_championnat --command predict >> le_championnat.txt
+python main.py --league serie_a --command predict >> serie_a.txt
